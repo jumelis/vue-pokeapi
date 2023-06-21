310a551ca61d8507f5859e691c35f547ad075860
javier <ertoyos@gmail.com>
2023-06-21 14:18:03 +0200

    add

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,143 @@
-<script>
-import { pokeapi } from './api/pokeapi'
-
-export default {
-  name: 'App',
-  data () {
-    return {
-      pokemonData: {},
-      pokemonID: '',
-    }
-  },
-
-  methods: {
-    async searchPokemon () {
-      try {
-        const pokemonToFind = await fetch(`${pokeapi}/${this.pokemonID}`)
-        const pokemon = await pokemonToFind.json()
-        this.pokemonData = pokemon
-        console.log(pokemon)
-        return pokemon
-      } catch (error) {
-        alert('Pokemon no Encontrado')
-      }
-    }
-  }
-}
-</script>
-
 <template>
-  <header class="header">
-    <label for="pokemonInput">
-      Tipo, Nombre o ID:
-      <input
-        type="text"
-        id="pokemonInput"
-        v-model="pokemonID"
-      >
-    </label>
-    <button
-      class="searchButton"
-      v-on:click="searchPokemon"
-    >
-      Buscar
-    </button>
-  </header>
-
-  <main
-    class="main" 
-    v-if="Object.entries(pokemonData).length > 0"
-  >
-    <section class="pokemonCard">
-      <div class="nameImage">
-        <h1 class="pokemonName">{{ pokemonData.name }}</h1>
-        <img
-          :src="pokemonData.sprites.front_default"
-          :alt="pokemonData.name"
-        >
+  <div id="app">
+    <div class="poke-container">
+      <div class="pokemon" v-for="pokemon in pokemons" :style="{ backgroundColor: getColor(pokemon) }">
+        <div class="img-container">
+          <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+        </div>
+        <div class="info">
+          <span class="number">{{ formatNumber(pokemon.id) }}</span>
+          <h3 class="name">{{ pokemon.name }}</h3>
+          <small class="type">Type: <span>{{ getPokemonType(pokemon) }}</span></small>
+        </div>
       </div>
-      <ul class="type">
-        <h2>Tipo:</h2>
-        <li
-          v-for="(type,key) in pokemonData.types"
-          :key="key"
-          :class="type.type.name"
-        >
-          <span>{{ type.type.name }}</span>
-        </li>
-      </ul>
-      <ul class="stats">
-        <h2>Estadisticas:</h2>
-        <li
-          v-for="(stat, key) in pokemonData.stats"
-          :key="key"
-        >
-          <span>{{ stat.stat.name}} -> {{stat.base_stat }}</span>
-        </li>
-      </ul>
-    </section>
-  </main>
+    </div>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      pokemons: []
+    };
+  },
+  mounted() {
+    this.fetchPokemon();
+  },
+  methods: {
+    async fetchPokemon() {
+      const pokemonCount = 150;
+      const apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+      for (let i = 1; i <= pokemonCount; i++) {
+        try {
+          const response = await fetch(apiUrl + i);
+          const pokemon = await response.json();
+          this.createPokemonCard(pokemon);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    },
+    createPokemonCard(pokemon) {
+      this.pokemons.push(pokemon);
+    },
+    getColor(pokemon) {
+      const colors = {
+        fire: '#FDDFDF',
+        grass: '#DEFDE0',
+        electric: '#FCF7DE',
+        water: '#DEF3FD',
+        ground: '#f4e7da',
+        rock: '#d5d5d4',
+        fairy: '#fceaff',
+        poison: '#98d7a5',
+        bug: '#f8d5a3',
+        dragon: '#97b3e6',
+        psychic: '#eaeda1',
+        flying: '#F5F5F5',
+        fighting: '#E6E0D4',
+        normal: '#F5F5F5'
+      };
+      const types = pokemon.types.map(type => type.type.name);
+      const type = types[0];
+      return colors[type];
+    },
+    formatNumber(number) {
+      return number.toString().padStart(3, '0');
+    },
+    getPokemonType(pokemon) {
+      const types = pokemon.types.map(type => type.type.name);
+      return types.join(', ');
+    }
+  }
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Lato:300,400&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background: #efefbb;
+  background: linear-gradient(to right, #d4d3dd, #efefbb);
+  font-family: "Lato", sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+}
+
+h1 {
+  letter-spacing: 3px;
+}
+
+.poke-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 20px;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.pokemon {
+  background-color: #eee;
+  border-radius: 10px;
+  box-shadow: 0 3px 15px rgba(100, 100, 100, 0.5);
+  padding: 20px;
+  text-align: center;
+}
+
+.pokemon .img-container {
+  background-color: rgba(255, 255, 255, 0.6);
+  border-radius: 50%;
+  width: 120px;
+  height: 120px;
+  text-align: center;
+  margin: 0 auto;
+}
+
+.pokemon .img-container img {
+  max-width: 90%;
+  margin-top: 20px;
+}
+
+.pokemon .info {
+  margin-top: 20px;
+}
+
+.pokemon .info .number {
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 5px 10px;
+  border-radius: 10px;
+  font-size: 0.8em;
+}
+
+.pokemon .info .name {
+  margin: 15px 0 7px;
+  letter-spacing: 1px;
+}
+</style>
