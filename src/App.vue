6779dea665d04fac4dce64c57cad1e1dee517866
Javier <alejandranaveshevia@MacBook-Air-de-Alejandra.local>
2023-06-22 15:03:46 +0200

    add-fit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
-    <div class="poke-container">
-      <div class="pokemon" v-for="pokemon in pokemons" :style="{ backgroundColor: getColor(pokemon) }">
-        <div class="img-container">
-          <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+    <div class="tarjetas">
+      <div class="tarjeta" v-for="pokemon in pokemons" :key="pokemon.id">
+        <div class="frontal" :style="{ backgroundColor: getColor(pokemon) }">
+          <div class="contenido">
+            <div class="img-container">
+              <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+            </div>
+            <h3 class="name">{{ pokemon.name }}</h3>
+            <small class="type">Type: <span>{{ getPokemonType(pokemon) }}</span></small>
+          </div>
         </div>
-        <div class="info">
-          <span class="number">{{ formatNumber(pokemon.id) }}</span>
-          <h3 class="name">{{ pokemon.name }}</h3>
-          <small class="type">Type: <span>{{ getPokemonType(pokemon) }}</span></small>
+        <div class="tracero">
+          <div class="contenido">
+            <p>Texto en la parte de atrás.</p>
+          </div>
         </div>
       </div>
     </div>
@@ -27,7 +33,7 @@ export default {
   },
   methods: {
     async fetchPokemon() {
-      const pokemonCount = 150;
+      const pokemonCount = 151;
       const apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
       for (let i = 1; i <= pokemonCount; i++) {
         try {
@@ -63,9 +69,6 @@ export default {
       const type = types[0];
       return colors[type];
     },
-    formatNumber(number) {
-      return number.toString().padStart(3, '0');
-    },
     getPokemonType(pokemon) {
       const types = pokemon.types.map(type => type.type.name);
       return types.join(', ');
@@ -73,7 +76,6 @@ export default {
   }
 };
 </script>
-
 <style scoped>
 @import url("https://fonts.googleapis.com/css?family=Lato:300,400&display=swap");
 
@@ -96,48 +98,122 @@ h1 {
   letter-spacing: 3px;
 }
 
-.poke-container {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-  gap: 20px;
-  max-width: 1200px;
-  margin: 0 auto;
+.tarjetas {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
 }
-.pokemon {
-  background-color: #eee;
+.tarjeta {
+  width: 200px;
+  margin: 1rem;
+
+  perspective: 1000px;
+  transform-style: preserve-3d;
+}
+
+.tarjeta .frontal,
+.tarjeta .tracero {
+  height: auto;
+  min-height: 280px;
+
+  background-size: cover;
+  background-position: center;
+
   border-radius: 10px;
-  box-shadow: 0 3px 15px rgba(100, 100, 100, 0.5);
-  padding: 20px;
-  text-align: center;
-}
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.25);
 
-.pokemon .img-container {
-  background-color: rgba(255, 255, 255, 0.6);
-  border-radius: 50%;
-  width: 120px;
-  height: 120px;
+  color: #fff;
+  font-size: 1.5rem;
   text-align: center;
+
+  backface-visibility: hidden;
+  transform-style: preserve-3d;
+  transition: transform .7s cubic-bezier(0.4, 0.2, 0.2, 1);
+}
+.tarjeta .frontal {
+  background-image: linear-gradient(
+      rgba(0, 0, 0, 0.5),
+      rgba(0, 0, 0, 0.5)
+    ), url(https:unsplash.it/500/500/);
+  background-color: #66D7D1;
+  /* Posicion por defecto */
+  transform: rotateY(0deg);
+}
+.tarjeta .tracero {
+  width: 100%;
+
+  background-color: #957DAD;
+
+  top: 0;
+  left: 0;
+  position: absolute;
+  /* Posicion por defecto */
+  transform: rotateY(180deg);
+}
+/* Velocidad de trancicion */
+.tarjeta:hover .frontal,
+.tarjeta:hover .tracero {transition: transform .7s cubic-bezier(0.4, 0.2, 0.2, 1);}
+
+/* Girar imagen */
+.tarjeta:hover .frontal {transform: rotateY(-180deg);}
+.tarjeta:hover .tracero {transform: rotateY(0deg);}
+
+
+.tarjeta .contenido {
+  width: 100%;
+  position: absolute;
+
+  padding: 2rem;
+  box-sizing: border-box;
+
+  top: 50%;
+  left: 0;
+  perspective: inherit;
+  /* Posicion de texto central */
+  transform: translateY(-50%) translateZ(60px) scale(0.94);
+}
+.frontal .contenido p {
+  font-size: 2rem;
+  position: relative;
+  margin-bottom: .5rem;
+}
+.frontal .contenido hr {
+  width: 4rem;
+  height: 1px;
+
+  background: #C6D4DF;
+
   margin: 0 auto;
+  margin-bottom: 1.5rem;
+}
+.frontal .contenido span { color: rgba(255, 255, 255, 0.7); }
+.frontal .contenido small { color: #E1ECF2; font-size: .8rem; }
+.frontal .contenido small span {
+  display: inline-block;
+  background-color: #E1ECF2;
+  padding: 2px 5px;
+  border-radius: 2px;
+  margin-left: .3rem;
+  font-size: .75rem;
 }
 
-.pokemon .img-container img {
-  max-width: 90%;
-  margin-top: 20px;
+.tracero .contenido {
+  padding: 2rem;
+  transform: translateY(-50%) translateZ(-60px);
+  background-color: #957DAD;
+  backface-visibility: hidden;
 }
-
-.pokemon .info {
-  margin-top: 20px;
+.tracero .contenido p { font-size: 1.2rem; }
+.tracero .contenido a {
+  color: #AAB8C2;
+  text-decoration: none;
+  background-color: #6F84A6;
+  padding: 8px 10px;
+  border-radius: 5px;
+  transition: background-color .2s ease, color .2s ease;
 }
-
-.pokemon .info .number {
-  background-color: rgba(0, 0, 0, 0.1);
-  padding: 5px 10px;
-  border-radius: 10px;
-  font-size: 0.8em;
-}
-
-.pokemon .info .name {
-  margin: 15px 0 7px;
-  letter-spacing: 1px;
+.tracero .contenido a:hover {
+  background-color: #5B6F9D;
+  color: #FFF;
 }
 </style>
