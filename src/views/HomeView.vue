c9e32da99811bdd4a6b819abc09256e11d8c5fd5
HabibullahAfzali <hbafzali@gmail.com>
2023-06-28 09:38:12 +0200

    Search functionality added to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,77 @@
+
 <template>
   <div>
-    <script-home></script-home>
+    <header>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="pokemonID">
+        <button class="btn btn-outline-success" type="button" @click="searchPokemon">Search</button>
+      </form>
+    </header>
+    <main>
+      <div v-if="filteredPokemon">
+        <h2>{{ filteredPokemon.name }}</h2>
+        <img :src="filteredPokemon.sprites.front_default" :alt="filteredPokemon.name" />
+        <ul>
+          <li v-for="type in filteredPokemon.types" :key="type.type.name">
+            {{ type.type.name }}
+          </li>
+        </ul>
+        <ul>
+          <li v-for="stat in filteredPokemon.stats" :key="stat.stat.name">
+            {{ stat.stat.name }} -> {{ stat.base_stat }}
+          </li>
+        </ul>
+      </div>
+
+      <div v-else>
+        <SubComponent :pokemons="pokemons" :getColor="getColor" :formatNumber="formatNumber" :getPokemonType="getPokemonType" />
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import ScriptHome from '../components/script-home.vue'
+import SubComponent from '../components/subcomponente-tem-style.vue'
+import { fetchPokemon, getColor, formatNumber, getPokemonType } from '../api/script-card'
+import { pokeapi } from '../api/pokeapi';
 
 export default {
+  name: 'HomeView',
   components: {
-    ScriptHome
+    SubComponent
+  },
+  data() {
+    return {
+      pokemons: [],
+      pokemonID: '',
+      filteredPokemon: null
+    };
+  },
+  async mounted() {
+    this.pokemons = await fetchPokemon();
+  },
+  methods: {
+    getColor,
+    formatNumber,
+    getPokemonType,
+    async searchPokemon() {
+      try {
+        const pokemonToFind = await fetch(`${pokeapi}/${this.pokemonID}`);
+        const pokemon = await pokemonToFind.json();
+        console.log(pokemon);
+        this.filteredPokemon = pokemon;
+      } catch (error) {
+        alert('Pokemon Id not exist!');
+      }
+    },
   }
 }
 </script>
+<style scoped>
+.d-flex{
+      display: flex !important;
+      margin-bottom: 1rem;
+      width: 30%;
+      margin-left: 70%;
+}
+</style>
--- src/views/NavBar.vue
+++ src/views/NavBar.vue
@@ -1,40 +1,85 @@
 <template>
-    <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+        </div>
+      </div>
+    </nav>
+     <main class="main" v-if="Object.keys(pokemonData).length > 0">
+        <section class="pokemonCard">
+          <div class="nameImage">
+            <h1 class="pokemonName">{{ pokemonData.name }}</h1>
+            <img :src="pokemonData.sprites.front_default" :alt="pokemonData.name">
+          </div>
+          <ul class="type">
+            <h2>Type:</h2>
+            <li v-for="(type, key) in pokemonData.types" :key="key" :class="type.type.name">
+              <span>{{ type.type.name }}</span>
+            </li>
+          </ul>
+          <ul class="stats">
+            <h2>Statistics:</h2>
+            <li v-for="(stat, key) in pokemonData.stats" :key="key">
+              <span>{{ stat.stat.name }} -> {{ stat.base_stat }}</span>
+            </li>
+          </ul>
+        </section>
+      </main>
   </div>
-</nav>
-    </div>
 </template>
+
+<script>
+import { pokeapi } from '../api/pokeapi'
+
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      pokemonData: {},
+      pokemonID: '',
+    }
+  },
+  methods: {
+    async searchPokemon() {
+      try {
+        const pokemonToFind = await fetch(`${pokeapi}/${this.pokemonID}`)
+        const pokemon = await pokemonToFind.json()
+        this.pokemonData = pokemon
+        console.log(pokemon)
+      } catch (error) {
+        alert('Pokemon not found')
+      }
+    }
+  }
+}
+</script>
