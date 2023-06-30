c0e7fdb36280aad85b8c8d8bdc112e5fe28a0b94
HabibullahAfzali <hbafzali@gmail.com>
2023-06-30 10:35:49 +0200

    Search add

--- src/router/index.js
+++ src/router/index.js
@@ -1,65 +1,60 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/search',
-      name: 'search',
-      component: () => import('../views/SearchView.vue')
+      path: "/FirstGen",
+      name: "FirstGen",
+      component: () => import("../components/First gen.vue"),
     },
     {
-      path: '/FirstGen',
-      name: 'FirstGen',
-      component: () => import('../components/First gen.vue')
+      path: "/SecondGen",
+      name: "SecondGen",
+      component: () => import("../components/Second gen.vue"),
     },
     {
-      path: '/SecondGen',
-      name: 'SecondGen',
-      component: () => import('../components/Second gen.vue')
+      path: "/ThirdGen",
+      name: "ThirdGen",
+      component: () => import("../components/Third gen.vue"),
     },
     {
-      path: '/ThirdGen',
-      name: 'ThirdGen',
-      component: () => import('../components/Third gen.vue')
+      path: "/FourthGen",
+      name: "FourthGen",
+      component: () => import("../components/fourth gen.vue"),
     },
     {
-      path: '/FourthGen',
-      name: 'FourthGen',
-      component: () => import('../components/fourth gen.vue')
+      path: "/FifthGen",
+      name: "FifthGen",
+      component: () => import("../components/Fifth gen.vue"),
     },
     {
-      path: '/FifthGen',
-      name: 'FifthGen',
-      component: () => import('../components/Fifth gen.vue')
+      path: "/SixthGen",
+      name: "SixthGen",
+      component: () => import("../components/Sixth gen.vue"),
     },
     {
-      path: '/SixthGen',
-      name: 'SixthGen',
-      component: () => import('../components/Sixth gen.vue')
+      path: "/SeventhGen",
+      name: "SeventhGen",
+      component: () => import("../components/Seventh gen.vue"),
     },
     {
-      path: '/SeventhGen',
-      name: 'SeventhGen',
-      component: () => import('../components/Seventh gen.vue')
+      path: "/EightGen",
+      name: "EightGen",
+      component: () => import("../components/Eighth generation.vue"),
     },
     {
-      path: '/EightGen',
-      name: 'EightGen',
-      component: () => import('../components/Eighth generation.vue')
+      path: "/NinthGen",
+      name: "NinthGen",
+      component: () => import("../components/Ninth generation.vue"),
     },
-    {
-      path: '/NinthGen',
-      name: 'NinthGen',
-      component: () => import('../components/Ninth generation.vue')
-    },
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,103 @@
+<template>
+  <div>
+    <header>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="pokemonID">
+        <button class="btn btn-outline-success" type="button"
+          @click="searchButtonText === 'Reset' ? resetPage() : searchPokemon()"> {{ searchButtonText }}</button>
+      </form>
+    </header>
+    <main>
+      <div v-if="filteredPokemon">
+        <h2>{{ filteredPokemon.name }}</h2>
+        <img :src="filteredPokemon.sprites.front_default" :alt="filteredPokemon.name" />
+        <ul>
+          <li v-for="type in filteredPokemon.types" :key="type.type.name">
+            {{ type.type.name }}
+          </li>
+        </ul>
+        <ul>
+          <li v-for="stat in filteredPokemon.stats" :key="stat.stat.name">
+            {{ stat.stat.name }} -> {{ stat.base_stat }}
+          </li>
+        </ul>
+      </div>
+
+      <div v-else>
+        <ScriptHome :pokemons="pokemons" :getColor="getColor" :formatNumber="formatNumber"
+          :getPokemonType="getPokemonType" />
+      </div>
+    </main>
+  </div>
+</template>
+
 <script>
 import ScriptHome from '../components/script-home.vue'
+import SubComponent from '../components/subcomponente-tem-style.vue'
+import { fetchPokemon, getColor, formatNumber, getPokemonType } from '../api/script-card'
+import { pokeapi } from '../api/pokeapi';
 
 export default {
+  name: 'HomeView',
   components: {
-    ScriptHome
+    ScriptHome,
+    SubComponent
+  },
+  data() {
+    return {
+      pokemons: [],
+      pokemonID: '',
+      filteredPokemon: null
+    };
+  },
+  computed: {
+    searchButtonText() {
+      return this.filteredPokemon ? 'Reset' : 'Search';
+    }
+  },
+  async mounted() {
+    this.pokemons = await fetchPokemon();
+  },
+  methods: {
+    getColor,
+    formatNumber,
+    getPokemonType,
+    async searchPokemon() {
+      if (this.filteredPokemon) {
+
+        this.pokemonID = '';
+        this.filteredPokemon = null;
+
+      }
+      else {
+
+        try {
+          const pokemonToFind = await fetch(`${pokeapi}/${this.pokemonID}`);
+          const pokemon = await pokemonToFind.json();
+          console.log(pokemon);
+          this.filteredPokemon = pokemon;
+        } catch (error) {
+          alert('Pokemon ID does not exist!');
+
+
+        }
+      }
+    },
+    resetPage() {
+      window.location.reload();
+    }
   }
 }
 </script>
-
-<template>
-  <main>
-      <script-home></script-home>
-  </main>
-</template>
+<style scoped>
+.d-flex {
+  display: flex !important;
+  margin-bottom: 1rem;
+  align-items: center;
+  margin-left: 60%;
+  margin-right: 4rem;
+}
+.btn {
+  background-color: black;
+}
+</style>
